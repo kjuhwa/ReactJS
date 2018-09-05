6d73312792266799f90bf02d6d131ab909ffdaaf
kjuhwa <kjuhwa@gmail.com>
2018-09-05 14:47:33 +0900

    기본 끝났다네...

---
+++ 1day/basic/mod.js
@@ -0,0 +1,12 @@
+// 모듈화의 대상
+// 개별 모듈화 : 변수, 함수
+// 객체 모듈화 : 객체 ( 개별 모듈화를 감싼 느낌)
+var name = 'ncia'
+// 개별 모듈화
+exports.name2 = 'ncia2'
+function a(){
+    return name
+}
+exports.a2 = function(){
+    return name
+}
